92add37f5ab90ad809ba115392e402b3cde5aaf0
Introverted <cruzjocelynb@gmail.com>
2016-09-02 18:26:33 -0700

    Update GameOver.cpp

--- GameOver.cpp
+++ GameOver.cpp
@@ -1,3 +1,4 @@
+///////////////// Jocelyn Cruz /////////////////
 #include "GameOver.h"
 
 Scene* GameOver::createGameOver()
@@ -39,15 +40,6 @@ bool GameOver::init()
 	gameover->setPosition(Vec2((int)winSizeWidth - 10, (int)winSizeHeight + 120));
 	this->addChild(gameover, 0);
 
-	joyListener = EventListenerJoystick::create();
-	joyListener->onEvent = CC_CALLBACK_1(GameOver::Joystick, this);
-	_eventDispatcher->addEventListenerWithFixedPriority(joyListener, 1);
-
-	if (MUSIC_ON)
-	{
-		experimental::AudioEngine::play2d("\\res\\sound\\music\\win_music.mp3", true, 0.5f);
-	}
-
 	return true;
 }
 
@@ -71,35 +63,6 @@ void GameOver::menuCloseCallback(Ref* pSender)
 #endif
 }
 
-void GameOver::Joystick(cocos2d::Event* event)
-{
-	EventJoystick* e = (EventJoystick*)event;
-	//CCLOG("JOYSTICK PRESENT");
-	bool present = e->isPresent();
-	//CCLOG(std::to_string(present).c_str());
-	if (present)
-	{
-		//CCLOG(e->getName());
 
-		int forbutton;
-		const unsigned char* buttonval = e->getButtonValues(&forbutton);
-		unsigned char b0 = buttonval[0];
-		unsigned char b1 = buttonval[1];
-		unsigned char b2 = buttonval[2];
-		unsigned char b3 = buttonval[3];
-
-		if (b0 || b1 || b2 || b3)
-		{
-			menu_item->selected();
-			button1 = true; //for key_release code
-		}
-
-		if (button1 == true && !b0 && !b1 && !b2 && !b3) //button was pushed then released
-		{
-			button1 = false;
-			menu_item->activate();
-		}
-	}
-}
 
 
